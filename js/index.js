fad5e86744dab5d947079696461f4c1f73c73f21
Nicolas Marini <nmarini44@gmail.com>
2019-05-11 19:00:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,6 +6,13 @@ function getRepositories() {
 
 function showRepositories() {
   console.log(this.responseText);
+  let repoList = '<ul>';
+  for (car i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getRepositories() {
@@ -14,3 +21,4 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
